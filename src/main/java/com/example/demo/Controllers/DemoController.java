554730bf87bf0f078d017581f7543f77c302dd06
Cynthia <cynthiaholman@outlook.com>
2019-07-30 20:06:48 -0500

    updated the controller

--- src/main/java/com/example/demo/Controllers/DemoController.java
+++ src/main/java/com/example/demo/Controllers/DemoController.java
@@ -1,4 +1,18 @@
 package com.example.demo.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class DemoController {
+    @RequestMapping(value = "welcome")
+   // @ResponseBody
+    public String loadIndexPage(){
+        return "index.html";
+    }
+
 }
